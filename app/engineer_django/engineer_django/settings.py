4b65c046a6518f0d81290dbe401c730183eed908
Alexander Gorelik <vlgamer@mail.ru>
2021-09-29 15:55:45 +0300

    Update settings.py

--- app/engineer_django/engineer_django/settings.py
+++ app/engineer_django/engineer_django/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from django.utils.translation import gettext_lazy as _
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
